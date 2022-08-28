43afb87ccd587dd5034e745a6a22622da8fb6704
Bahjat Khan <bahjat_khan2002@yahoo.com>
2022-08-28 02:09:06 -0400

    Predicate and Function practice codes.

--- project/src/main/java/function/RemovingWhiteSpace.java
+++ project/src/main/java/function/RemovingWhiteSpace.java
@@ -1,15 +1,16 @@
 package function;
 
+import java.sql.SQLOutput;
 import java.util.function.Function;
 
 public class RemovingWhiteSpace {
     public static void main(String[] args) {
         String name = "   Schwab   4";
-        Function<String, String> s = p -> p.replaceAll("   ", "");
+        Function<String, String> s = p -> p.replaceAll("\\s", "");
         System.out.println(s.apply(name));
 
         String shopName= "The meat shop             ";
-        Function<String, Integer> s1 = p -> p.length() - p.replaceAll(" ","").length();
+        Function<String, Integer> s1 = p -> p.length() - p.replaceAll("\\s","").length();
         System.out.println(s1.apply(shopName));
     }
 }
