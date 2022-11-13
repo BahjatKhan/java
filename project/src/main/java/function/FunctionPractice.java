17a94e48193060583c91ff8f07b567f2c634ac82
Bahjat Khan <bahjat_khan2002@yahoo.com>
2022-11-13 09:15:04 -0500

    Predicate and Function practice codes.

--- project/src/main/java/function/FunctionPractice.java
+++ project/src/main/java/function/FunctionPractice.java
@@ -4,13 +4,8 @@ import java.util.function.Function;
 
 public class FunctionPractice {
     public static void main(String[] args) {
-//        Function<String, Integer> s = o -> o.length();
-//        System.out.println(s.apply("durga"));
-        int i;
-        for(i=0;i<5;i--){
-
-        }
-        System.out.println(i);
+        Function<String, Integer> s = o -> o.length();
+        System.out.println(s.apply("durga"));
     }
 
 
